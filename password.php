f0fb625f7d7a341e635b8b6bf255f431df16779b
thetnaisoe <holyhemish02@gmail.com>
2023-11-26 01:06:21 +0100

    adding comments how post method works

--- password.php
+++ password.php
@@ -6,7 +6,10 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="password.php" method="post">
+    <form action="password.php" method="post"> 
+        
+        /* trying post method instead of get. The main difference is that you cannot see data in url hence protecting the data from the public*/
+
         Password: <input type="password" name="password"> <br>
         <input type="submit">
 
